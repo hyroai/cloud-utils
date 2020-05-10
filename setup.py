6da651844ccc6b8b60ec09b6395c6ba28cb4b718
david <david@airbud.io>
2020-05-10 16:15:05 +0300

    add pre-commit

--- setup.py
+++ setup.py
@@ -11,8 +11,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_namespace_packages(),
     zip_safe=False,
-    install_requires=[
-        "azure-storage-blob==2.1.0",
-        "google-cloud-storage",
-    ],
+    install_requires=["azure-storage-blob==2.1.0", "google-cloud-storage"],
 )
