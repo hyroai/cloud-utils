ed11ea10825898ba80815dfd2e9dab965935a1a9
david <david@airbud.io>
2020-05-11 10:10:00 +0300

    removing default value from MONGODB_URI in config.py

--- cloud_utils/config.py
+++ cloud_utils/config.py
@@ -9,6 +9,4 @@ BUCKET_NAME = os.getenv("BUCKET_NAME", "nlu-cache")
 EXTERNAL_API_CACHE_REDIS_HOST = os.getenv("EXTERNAL_API_CACHE_REDIS_HOST", "10.0.16.12")
 EXTERNAL_API_CACHE_REDIS_PASSWORD = os.getenv("EXTERNAL_API_CACHE_REDIS_PASSWORD")
 
-MONGODB_URI = os.getenv(
-    "MONGODB_URI", "mongodb+srv://airbud:jPr7mkipwTsLQMz@airbud-dnate.gcp.mongodb.net"
-)
+MONGODB_URI = os.getenv("MONGODB_URI")
