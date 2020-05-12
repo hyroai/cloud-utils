4b5ec5bb1fde0d7b1234809426badfdc30c2665a
Nur Lan <nurxlan@gmail.com>
2020-05-12 22:50:54 +0300

    Add `download_blob_as_binary` (#2)
    Add `download_blob_as_binary`

--- cloud_utils/storage/azure.py
+++ cloud_utils/storage/azure.py
@@ -21,12 +21,20 @@ def upload_blob(bucket_name: Text, blob_name: Text, obj: Any):
 
 
 @toolz.curry
-def download_blob_as_string(bucket_name: Text, blob_name: Text):
+def download_blob_as_string(bucket_name: Text, blob_name: Text) -> Text:
     return (
         _get_blob_service()
         .get_blob_to_text(bucket_name, blob_name, encoding="utf-8")
         .content
     )
+
+
+@toolz.curry
+def download_blob_as_stream(bucket_name: Text, blob_name: Text) -> io.BytesIO:
+    stream = io.BytesIO()
+    _get_blob_service().get_blob_to_stream(bucket_name, blob_name, stream)
+    stream.seek(0)
+    return stream
 
 
 def download_blob_to_file(bucket_name: Text, blob_name: Text, path: pathlib.Path):
