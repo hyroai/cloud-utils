166b2492865c2364986e4e8aee3d38e71f2f2305
Erez Haim <erezh.work@gmail.com>
2020-05-25 14:31:23 +0300

    Using setup.cfg

--- setup.py
+++ setup.py
@@ -1,23 +1,2 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    _LONG_DESCRIPTION = fh.read()
-
-
-setuptools.setup(
-    name="cloud-utils",
-    version="0.0.1",
-    long_description=_LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_namespace_packages(),
-    zip_safe=False,
-    install_requires=[
-        "azure-storage-blob==2.1.0",
-        "google-cloud-storage",
-        "pymongo",
-        "xmltodict",
-        "gamla",
-        "kubernetes",
-        "toolz",
-    ],
-)
+from setuptools import setup
+setup()
