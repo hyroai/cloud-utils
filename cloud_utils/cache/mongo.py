1295cab78081e39c09bb27e68f962c16eca0b303
david <david@airbud.io>
2020-09-30 10:17:26 +0300

    fix typing in mongo.py

--- cloud_utils/cache/mongo.py
+++ cloud_utils/cache/mongo.py
@@ -9,21 +9,21 @@ ASCENDING = pymongo.ASCENDING
 DESCENDING = pymongo.DESCENDING
 
 
-def get_client(mongodb_uri: Text, **kwargs):
+def get_client(mongodb_uri: Text, **kwargs) -> pymongo.MongoClient:
     return pymongo.MongoClient(mongodb_uri, **kwargs)
 
 
 @gamla.curry
 def aggregate(
     collection: pymongo.collection.Collection, aggregation: Iterable[Dict[Text, Any]],
-) -> Tuple[Dict, ...]:
+) -> pymongo.command_cursor.CommandCursor:
     return collection.aggregate(list(aggregation), allowDiskUse=True)
 
 
 @gamla.curry
 def find(
     query: Dict[Text, Any], collection: pymongo.collection.Collection,
-) -> Tuple[Dict, ...]:
+) -> pymongo.cursor.Cursor:
     return collection.find(query)
 
 
@@ -33,7 +33,7 @@ find_all = find({})
 @gamla.curry
 def sort(
     collection: pymongo.collection.Collection, key: Text, direction: int,
-) -> Tuple[Dict, ...]:
+):
     return collection.sort(key, direction)
 
 
@@ -52,17 +52,21 @@ def add_match_filter(f: Callable) -> Tuple[Dict, ...]:
     )
 
 
-def query_to_count_aggregation_stage() -> Dict[Text, Any]:
+def query_to_count_aggregation_stage() -> Dict[Text, Text]:
     return {"$count": "count"}
 
 
-def query_to_match_aggregation_stage(query: Dict[Text, Any]) -> Dict[Text, Any]:
+def query_to_match_aggregation_stage(
+    query: Dict[Text, Any],
+) -> Dict[Text, Dict[Text, Any]]:
     return {"$match": query}
 
 
-def query_to_sample_aggregation_stage(n: int) -> Dict:
+def query_to_sample_aggregation_stage(n: int) -> Dict[Text, Dict[Text, int]]:
     return {"$sample": {"size": n}}
 
 
-def query_to_sort_aggregation_stage(query: Dict[Text, int]) -> Dict[Text, Any]:
+def query_to_sort_aggregation_stage(
+    query: Dict[Text, int],
+) -> Dict[Text, Dict[Text, int]]:
     return {"$sort": query}
