7e1723ef38d69bdcd5d1498af7197df4b3f6e1f8
Eliott <eliott.ifergan@gmail.com>
2020-11-04 10:24:56 +0100

    remove analysis dependency

--- cloud_utils/cache/motor.py
+++ cloud_utils/cache/motor.py
@@ -1,12 +1,8 @@
-import datetime
 from typing import Text
 
-import pymongo
-from cloud_utils.cache import mongo
-from motor import motor_asyncio
-
-from analysis import fields
 import gamla
+import pymongo
+from motor import motor_asyncio
 from toolz import curried
 
 
@@ -28,26 +24,3 @@ async def cursor_to_list(length: int, cursor: motor_asyncio.AsyncIOMotorCursor):
         return await cursor.to_list(length=length)
     except pymongo.errors.ExecutionTimeout:
         return ()
-
-
-@gamla.curry
-async def query_events(
-    event_type: Text,
-    bot_id: Text,
-    collection: motor_asyncio.AsyncIOMotorCollection,
-    timestamp: datetime.datetime,
-):
-    cursor = collection.find(
-        {
-            fields.EVENT_TYPE: event_type,
-            fields.CREATED_AT: {"$lte": timestamp},
-            fields.BOT_ID: bot_id,
-        },
-        max_time_ms=6000,
-    ).sort(fields.CREATED_AT, mongo.DESCENDING)
-    try:
-        return await cursor.to_list(length=100)
-    except pymongo.errors.ExecutionTimeout:
-        return ()
-
-
