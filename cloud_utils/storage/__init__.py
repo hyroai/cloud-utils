297f825c2c70019ba88de3d7581a5d302fad0e06
david <david@airbud.io>
2021-03-15 12:16:49 +0200

    remove default from curry in download_blob_as_string

--- cloud_utils/storage/__init__.py
+++ cloud_utils/storage/__init__.py
@@ -4,6 +4,10 @@ import os
 try:
     if os.getenv("STORAGE_PROVIDER", "azure") == "azure":
         from cloud_utils.storage import azure as _storage_service
+
+        download_blob_as_string_with_encoding = (
+            _storage_service.download_blob_as_string_with_encoding
+        )
     elif os.getenv("STORAGE_PROVIDER") == "gcp":
         from cloud_utils.storage import gcp as _storage_service  # type: ignore
     else:
