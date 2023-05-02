2f1e6120f0838773951df3d9e3f45644f379cac6
Yoni Isaacs <yoni@hyro.ai>
2023-05-02 15:21:37 +0300

    redis test for ttl

--- cloud_utils/cache/stores/redis_test.py
+++ cloud_utils/cache/stores/redis_test.py
@@ -1,14 +1,16 @@
+import asyncio
+
 from fakeredis import FakeServer, aioredis
 
+from cloud_utils.cache import utils
 from cloud_utils.cache.stores import redis
 
 _SERVER = FakeServer()
-_REDIS = aioredis.FakeRedis(server=_SERVER)
 
 
 async def test_redis_store_unbounded():
-
-    get_item, set_item = redis.make_store(_REDIS, "unbound_store", 0)
+    client = aioredis.FakeRedis(server=_SERVER)
+    get_item, set_item = redis.make_store(client, "unbound_store", 0)
 
     await set_item("1", 1)
     await set_item("2", 2)
@@ -17,3 +19,15 @@ async def test_redis_store_unbounded():
     assert await get_item("1") == 1
     assert await get_item("2") == 2
     assert await get_item("3") == "3"
+
+
+async def test_redis_store_ttl():
+    client = aioredis.FakeRedis(server=_SERVER)
+    get_item, set_item = redis.make_store(client, "ttl_1", 1)
+
+    await set_item("1", 1)
+    await asyncio.sleep(2)
+    try:
+        await get_item("1")
+    except KeyError:
+        assert utils.cache_key_name("ttl_1", "1") not in await client.keys()
