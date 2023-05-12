d475361219a65d8ed48bd5bf7c94bfb1545e49e2
erez-work <64847477+erez-work@users.noreply.github.com>
2023-05-12 07:40:54 -0400

    Adding the option to add extra parameters to helm command (#65)
    * Adding the option to add extra parameters to helm command
    
    * pre-commit

--- cloud_utils/k8s/helm_connector.py
+++ cloud_utils/k8s/helm_connector.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import os
 import tempfile
-from typing import Any, Dict, Text
+from typing import Any, Dict, Iterable, Text
 
 import gamla
 import yaml
@@ -39,6 +39,7 @@ async def install_release(
     release_name: str,
     chart_values: Dict[Text, Any],
     chart_physical_dir: str,
+    helm_extra_args: Iterable[str],
 ):
     handle, filename = tempfile.mkstemp()
     del handle
@@ -46,7 +47,8 @@ async def install_release(
         with open(filename, "w") as values_file:
             values_file.write(yaml.dump(chart_values))
         await _run_in_shell(
-            ["helm", "upgrade", release_name, chart_name, "--install", "-f", filename],
+            ["helm", "upgrade", release_name, chart_name, "--install", "-f", filename]
+            + helm_extra_args,
             chart_physical_dir,
         )
     finally:
