69499d60f477665f6f3059d9cf46341b587f5089
adirose <adi.r@hyro.ai>
2023-06-11 14:30:12 +0300

    don't perform json loads if the stored object is a bytes object

--- cloud_utils/cache/stores/redis_sync.py
+++ cloud_utils/cache/stores/redis_sync.py
@@ -2,11 +2,10 @@ import json
 import logging
 from typing import Callable, Tuple
 
+import gamla
 import redis
 
 from cloud_utils.cache import utils
-import zlib
-import gamla
 
 
 def _redis_error_handler(f):
@@ -23,36 +22,34 @@ def _redis_error_handler(f):
     return wrapper
 
 
-def _make_store(
-    zip_content: bool, redis_client: redis.Redis, ttl: int, name: str, force: bool
+def make_store(
+    redis_client: redis.Redis,
+    ttl: int,
+    name: str,
 ) -> Tuple[Callable, Callable]:
     utils.log_initialized_cache("redis", name)
 
     def get_item(key: str):
-        if force:
-            raise KeyError
         cache_key = utils.cache_key_name(name, key)
         result = _redis_error_handler(redis_client.get)(cache_key)
         if result is None:
             logging.debug(f"{key} is not in {name}")
             raise KeyError
         try:
-            if zip_content:
-                result = gamla.pipe(
-                    result, zlib.decompress, lambda x: x.decode("utf-8"), json.dumps
-                )
+            if type(result) is bytes:
+                return result
             return json.loads(result)
         except ValueError:  # Key contents are malformed (will force key to update).
             logging.error(f"Malformed key detected: {key} in {name}.")
             raise KeyError
 
     def set_item(key: str, value):
+        value = json.loads(value) if type(value) is not bytes else value
         if ttl == 0:
             _redis_error_handler(
                 redis_client.set,
-            )(utils.cache_key_name(name, key), json.dumps(value))
+            )(utils.cache_key_name(name, key), value)
         else:
-            value = zlib.compress(value) if zip_content else json.dumps(value)
             _redis_error_handler(redis_client.setex)(
                 utils.cache_key_name(name, key),
                 ttl,
@@ -60,15 +57,3 @@ def _make_store(
             )
 
     return get_item, set_item
-
-
-def make_store(
-    redis_client: redis.Redis, ttl: int, name: str, force: bool = False
-) -> Tuple[Callable, Callable]:
-    return _make_store(False, redis_client, ttl, name, force)
-
-
-def make_store_zip(
-    redis_client: redis.Redis, ttl: int, name: str, force: bool = False
-) -> Tuple[Callable, Callable]:
-    return _make_store(True, redis_client, ttl, name, force)
