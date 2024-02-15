8a12d173bd54e9a9d93588d5c7c1ecdd8112c07d
Eden Sonbolyan <edensonbolyan@Edens-MacBook-Pro.local>
2024-02-15 17:40:01 +0200

    fixes

--- cloud_utils/vault.py
+++ cloud_utils/vault.py
@@ -1,51 +1,58 @@
 from typing import Callable, Optional
 
-import hvac
-from hvac import exceptions
+from hvac import Client
 from hvac.api.auth_methods import Kubernetes
+from hvac.exceptions import InvalidPath
 
 _MOUNT_POINT = "secret"
 
 
-def _build_read_all_secrets(client: hvac.Client):
-    def read_all_secrets(path: str, version: Optional[str]) -> dict[str, str]:
-        read_secrets = client.secrets.kv.v2.read_secret_version(
-            path=path, version=version
-        )
-        return read_secrets["data"]["data"]
-
-    return read_all_secrets
+class InvalidVaultPath(Exception):
+    pass
 
 
-def _build_write_or_update_secrets(client: hvac.Client, read_all_secrets: Callable):
-    def write_or_update_secrets(path: str, secrets: dict[str, str]):
+def _build_read_secret(client: Client) -> Callable:
+    def read_secret(path: str, version: Optional[str]) -> dict[str, str]:
         try:
-            secrets_data = read_all_secrets(path, None)
-        except hvac.exceptions.InvalidPath:
-            secrets_data = {}
-        secrets_data.update(secrets)
+            secret = client.secrets.kv.v2.read_secret_version(
+                path=path, version=version
+            )
+            return secret["data"]["data"]
+        except InvalidPath:
+            raise InvalidVaultPath(f"Invalid vault path: {path}")
+
+    return read_secret
+
+
+def _build_write_or_update_secret(client: Client, read_secret: Callable) -> Callable:
+    def write_or_update_secret(path: str, new_keys: dict[str, str]):
+        try:
+            existing_keys = read_secret(path, None)
+        except InvalidVaultPath:
+            existing_keys = {}
+        existing_keys.update(new_keys)
         client.secrets.kv.v2.create_or_update_secret(
             path=path,
-            secret=secrets_data,
+            secret=existing_keys,
             mount_point=_MOUNT_POINT,
         )
 
-    return write_or_update_secrets
+    return write_or_update_secret
 
 
-def make_vault(host: str, token: Optional[str], role: Optional[str]):
+def make_vault(host: str, role: Optional[str], token: Optional[str]):
     if not token and not role:
         raise Exception("Must specify either token or role")
 
     if token:
-        client = hvac.Client(url=host, token=token)
+        client = Client(url=host, token=token)
     else:
-        client = hvac.Client(url=host)
+        client = Client(url=host)
         Kubernetes(client.adapter).login(
             role=role,
             jwt=open("/var/run/secrets/kubernetes.io/serviceaccount/token").read(),
         )
 
-    read_all_secrets = _build_read_all_secrets(client)
-    write_or_update_secrets = _build_write_or_update_secrets(client, read_all_secrets)
-    return read_all_secrets, write_or_update_secrets
+    read_secret = _build_read_secret(client)
+    write_or_update_secret = _build_write_or_update_secret(client, read_secret)
+    return read_secret, write_or_update_secret
